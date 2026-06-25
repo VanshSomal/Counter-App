bcb6005f4db9f8eec81ab646c50a1e668383b4b5
VanshSomal <vanshsomal20@gmail.com>
2026-06-25 22:45:14 +0530

    feat: Added custom Set Counter

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 let result=0;
 function increment(){
-    result++;
+    // grab the input element from the HTML
+    const stepInput=document.getElementById('customCounterInput');
+
+    // Get its current value. If it's empty default to 1
+    // We use Number() to convert the text value "5" into the actual number 5.
+    let stepSize =Number(stepInput.value);
+    if(stepSize<=0){
+        stepSize=1; //Fallback so the app doesn't break or freeze if empty/0
+    }
+    // 3. Instead of result++, add the dynamic step size.
+    result=result+stepSize;
+    document.querySelector('.js-alert').innerHTML = "";
+    // updateCounterUI();
     document.querySelector('.js-result').innerHTML=result;
-    document.querySelector('.js-alert').innerHTML="";
+
 }
 
 function decrement(){
-    if(result<=0){
+    // grab the input element from the HTML
+    const stepInput=document.getElementById('customCounterInput');
+    // 2 Get the step size (default to 1 if empty/0)
+    let stepSize =Number(stepInput.value);
+    if(stepSize<=0){
+        stepSize=1;
+    }
+    // 3.Validation:check if subtraacting thsi step size drops us below 0
+    if(result-stepSize<0){
         // alert("cannot decrement further");
         document.querySelector('.js-alert').innerHTML="Counter cannot be -ve"
         return;
     }
-        result--;
+        result=result-stepSize;
         document.querySelector('.js-result').innerHTML=result;
 }
 
@@ -34,8 +54,8 @@ function closeSettings(){
     modalOverlay.classList.add('hidden');
 }
 
-
 // Event Hooks
 gearBtn.addEventListener('click',openSettings);
 closeModalBtn.addEventListener('click',closeSettings);
 modalOverlay.addEventListener('click',closeSettings); //close menu if clicking outside teh box
+
